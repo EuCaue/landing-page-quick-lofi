85c3bd35eb7f978ad0b6e0f37f124bd9497f7895
EuCaue <souzacaue@proton.me>
2025-12-25 23:15:14 -0300

    feat: add mobile header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,17 @@
 import Link, { type LinkProps } from "next/link";
-import { GithubIcon, GlobeIcon, HeartHandshakeIcon } from "lucide-react";
+import {
+  GithubIcon,
+  GlobeIcon,
+  HeartHandshakeIcon,
+  MenuIcon,
+} from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 type HeaderActionType = LinkProps &
   React.AnchorHTMLAttributes<HTMLAnchorElement> & {
     children: React.ReactNode;
@@ -31,31 +43,52 @@ function HeaderAction({ href, children, ...rest }: HeaderActionType) {
   );
 }
 
-export default function Header() {
+function MobileHeader() {
   return (
-    <header
-      className="
-        sticky top-0 z-10
-        w-full
-        flex items-center gap-6
-        p-4
-        bg-(--header-bg)
-        text-(--header-fg)
-        border-b border-(--border-color)
-      "
-    >
-      <h1
-        className="font-bold text-2xl flex-1"
-        title="Open GNOME Extension Website"
-      >
-        <Link
-          href="https://extensions.gnome.org/extension/6904/quick-lofi/"
-          target="_blank"
-        >
-          QUICK LOFI
-        </Link>
-      </h1>
+    <span className="flex lg:hidden">
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <MenuIcon />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="bg-card text-card-foreground">
+          <DropdownMenuItem className="dark:focus:text-card-foreground">
+            <HeaderAction
+              href="https://extensions.gnome.org/extension/6904/quick-lofi/"
+              title="Open GNOME Extension Website"
+            >
+              <GlobeIcon />
+              Extension Website
+            </HeaderAction>
+          </DropdownMenuItem>
 
+          <DropdownMenuItem className="dark:focus:text-card-foreground">
+            <HeaderAction
+              href="https://github.com/eucaue/gnome-shell-extension-quick-lofi"
+              title="Open Extension Github Repository."
+            >
+              <GithubIcon />
+              GitHub
+           </HeaderAction>
+          </DropdownMenuItem>
+
+          <DropdownMenuItem className="dark:focus:text-card-foreground!">
+            <HeaderAction
+              href="https://ko-fi.com/eucaue"
+              title="Open Extension Ko-Fi Page."
+            >
+              <HeartHandshakeIcon />
+              Sponsor
+            </HeaderAction>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </span>
+  );
+}
+
+function DesktopHeader() {
+  return (
+    <span className="hidden lg:flex">
       <HeaderAction
         href="https://extensions.gnome.org/extension/6904/quick-lofi/"
         title="Open GNOME Extension Website"
@@ -79,6 +112,34 @@ export default function Header() {
         <HeartHandshakeIcon />
         Sponsor
       </HeaderAction>
+    </span>
+  );
+}
+
+export default function Header() {
+  return (
+    <header
+      className="
+        sticky top-0 z-10
+        w-full
+        flex items-center gap-4
+        p-4
+        bg-card text-card-foreground
+      "
+    >
+      <h1
+        className="font-bold text-2xl flex-1"
+        title="Open GNOME Extension Website"
+      >
+        <Link
+          href="https://extensions.gnome.org/extension/6904/quick-lofi/"
+          target="_blank"
+        >
+          QUICK LOFI
+        </Link>
+      </h1>
+      <DesktopHeader />
+      <MobileHeader />
     </header>
   );
 }
